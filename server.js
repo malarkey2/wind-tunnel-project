48c99999dae50c160d4ebe8c2f994be9cf4163f8
Malarkey02 <harshmathur2002@gmail.com>
2021-10-21 03:01:22 +0530

    Added Downloadable Files. Adding final touches before deploying

--- server.js
+++ server.js
@@ -19,6 +19,95 @@ app.get('/', (req, res) => {
   app.get('/build', (req, res) => {
     res.render('build');
   });
+
+
+
+  //File Downloads --- FILE DOWNLOADS --- File Downloads
+
+
+ // Flow Stabilizer
+ app.get('/download/flow-stabilizer-full', function(req, res){
+  const file = `${__dirname}/public/plan-pdfs/full/Flow-Stabilizer.pdf`;
+  res.download(file); // Set disposition and send it.
+});
+
+app.get('/download/flow-stabilizer-tiled', function(req, res){
+  const file = `${__dirname}/public/plan-pdfs/tiled/Flow-Stabilizer-tiled.pdf`;
+  res.download(file); // Set disposition and send it.
+});  
+
+
+  // Contraction Section
+
+  app.get('/download/contraction-base-full', function(req, res){
+    const file = `${__dirname}/public/plan-pdfs/full/Contraction-Base.pdf`;
+    res.download(file); // Set disposition and send it.
+  });
+
+  app.get('/download/contraction-base-tiled', function(req, res){
+    const file = `${__dirname}/public/plan-pdfs/tiled/Contraction-Base-tiled.pdf`;
+    res.download(file); // Set disposition and send it.
+  });  
+
+  app.get('/download/contraction-side-full', function(req, res){
+    const file = `${__dirname}/public/plan-pdfs/full/Contraction-Sides.pdf`;
+    res.download(file); // Set disposition and send it.
+  });
+
+  app.get('/download/contraction-side-tiled', function(req, res){
+    const file = `${__dirname}/public/plan-pdfs/tiled/Contraction-Base-tiled.pdf`;
+    res.download(file); // Set disposition and send it.
+  });
+
+  app.get('/download/contraction-hood-full', function(req, res){
+    const file = `${__dirname}/public/plan-pdfs/full/Contraction-Hood.pdf`;
+    res.download(file); // Set disposition and send it.
+  });
+
+  app.get('/download/contraction-hood-tiled', function(req, res){
+    const file = `${__dirname}/public/plan-pdfs/tiled/Contraction-Hood-tiled.pdf`;
+    res.download(file); // Set disposition and send it.
+  });
+
+  //Test Section
+  app.get('/download/test-section-full', function(req, res){
+    const file = `${__dirname}/public/plan-pdfs/full/Test-Section-Build.pdf`;
+    res.download(file); // Set disposition and send it.
+  });
+  
+  app.get('/download/test-section-tiled', function(req, res){
+    const file = `${__dirname}/public/plan-pdfs/tiled/Test-Section-tiled.pdf`;
+    res.download(file); // Set disposition and send it.
+  });    
+
+  //Diffuser
+
+  app.get('/download/diffuser-hood-full', function(req, res){
+    const file = `${__dirname}/public/plan-pdfs/full/Diffuser-Hood.pdf`;
+    res.download(file); // Set disposition and send it.
+  });
+  
+  app.get('/download/diffuser-hood-tiled', function(req, res){
+    const file = `${__dirname}/public/plan-pdfs/tiled/Diffuser-Hood-tiled.pdf`;
+    res.download(file); // Set disposition and send it.
+  }); 
+
+  app.get('/download/diffuser-side&base-full', function(req, res){
+    const file = `${__dirname}/public/plan-pdfs/full/Diffuser-Base-and-Sides.pdf`;
+    res.download(file); // Set disposition and send it.
+  });
+  
+  app.get('/download/diffuser-side&base-tiled', function(req, res){
+    const file = `${__dirname}/public/plan-pdfs/tiled/Diffuser-Base-and-Side-tiled.pdf`;
+    res.download(file); // Set disposition and send it.
+  }); 
+
+
+
+
+ 
+
+
   
   
   
